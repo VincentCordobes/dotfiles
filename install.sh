496ee8eb7124bc3ee50e6fc8dbac1d1722aea548
VincentCordobes <vincent.cordobes@gmail.com>
2018-03-10 15:05:48 +0100

    symlink vim and fish

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 mkdir -p ~/.config/nvim
+mkdir -p ~/.config/fish
+
+ln -sf $PWD/config.fish ~/.config/fish/config.fish
+ln -sf $PWD/init.vim ~/.config/nvim/init.vim
 
 if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
   echo "Installing vim-plug..."
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+  echo "Installing vim plugins..."
+  nvim +PlugInstall +qa
 fi
 
-echo "Installing vim plugins..."
-nvim +PlugInstall +qa
