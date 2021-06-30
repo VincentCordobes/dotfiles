3de88ddfb27a199b4905e1a1f4e7973e1704ad08
Vincent Cordobes <vincent.cordobes@gmail.com>
2021-06-30 09:21:02 +0200

    Simplify screenshot

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,8 @@
 
 echo "Installing scripts..."
 
-sudo ln -sf ~/dotfiles/scripts/record-screen-area /usr/local/bin/record-screen-area
+sudo ln -sf ~/dotfiles/scripts/record-screen-area  /usr/local/bin/record-screen-area
+sudo ln -sf ~/dotfiles/scripts/capture-screen      /usr/local/bin/capture-screen
+sudo ln -sf ~/dotfiles/scripts/capture-screen-area /usr/local/bin/capture-screen-area
 
 echo "Done."
