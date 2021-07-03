fbcee333a08e91199e7b948a96af23a2c6d2df54
Vincent Cordobes <vincent.cordobes@gmail.com>
2021-07-03 21:16:02 +0200

    Update

--- misc/install.sh
+++ misc/install.sh
@@ -10,4 +10,5 @@ ln -sf ~/dotfiles/misc/mpv.conf ~/.config/mpv/mpv.conf
 
 echo "XDG defaults"
 xdg-mime default zathura.desktop   application/pdf
+xdg-mime default zathura.desktop   application/epub+zip
 xdg-mime default imv.desktop       image/png
