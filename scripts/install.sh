3a9bc61a734c99a3137e50cd823ee603fc01366d
Vincent Cordobes <vincent.cordobes@gmail.com>
2021-07-04 11:55:35 +0200

    Update

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,4 +6,7 @@ sudo ln -sf ~/dotfiles/scripts/record-screen-area  /usr/local/bin/record-screen-
 sudo ln -sf ~/dotfiles/scripts/capture-screen      /usr/local/bin/capture-screen
 sudo ln -sf ~/dotfiles/scripts/capture-screen-area /usr/local/bin/capture-screen-area
 
+sudo ln -sf ~/dotfiles/scripts/howmuch-vat         /usr/local/bin/howmuch-vat
+sudo ln -sf ~/dotfiles/scripts/howmuch-cotsoc      /usr/local/bin/howmuch-cotsoc
+
 echo "Done."
