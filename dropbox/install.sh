51f247431f23afb8450a76c98f5738362503a322
Vincent Cordobes <vincent.cordobes@gmail.com>
2021-08-31 10:39:46 +0200

    Update dropbox service

--- dropbox/install.sh
+++ dropbox/install.sh
@@ -6,19 +6,7 @@ yay -S dropbox
 echo "Please proceed at the install manually"
 
 # $ systemctl --user edit dropbox.service  
-# put the unit in
 # $ systemctl --user enable dropbox.service
 
-# [Unit]
-# Description=Dropbox
-#
 # [Service]
-# Type=simple
-# ExecStart=
-# ExecStart=%h/.dropbox-dist/dropboxd 
-# ExecReload=/bin/kill -HUP $MAINPID
-# KillMode=process
-# Restart=on-failure
-#
-# [Install]
-# WantedBy=default.target
+# Environment=DISPLAY=:0
