bf04df8fffc1bfdf7cf983d5acc7cdbae2374804
Vincent Cordobes <vincent.cordobes@gmail.com>
2022-01-29 11:44:14 +0100

    update

--- sway/install.sh
+++ sway/install.sh
@@ -3,3 +3,4 @@ DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 ln -sf $DIRNAME/config ~/.config/sway/config
 ln -sf  ~/dotfiles/sway/kanshi ~/.config/kanshi/config
 ln -sf  ~/dotfiles/sway/sway-session.target ~/.config/systemd/user/sway-session.target
+ln -sf  ~/dotfiles/sway/swayidle.service ~/.config/systemd/user/swayidle.service
