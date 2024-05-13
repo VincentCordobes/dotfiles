ad7505a0a4996436d2bbc759b00c43d4e551b735
Vincent Cordobes <vincent.cordobes@gmail.com>
2024-05-13 09:13:39 +0200

    update

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,4 +5,3 @@ echo "Installing zsh"
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/zprofile ~/.zprofile
 ln -sf ~/dotfiles/zsh/zshenv ~/.zshenv
-ln -sf ~/dotfiles/zsh/profile ~/.profile
